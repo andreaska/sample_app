94093d05650cad2bb8a306db288b07319713378d
= <759487@beuth-hochschule.de>
2011-04-10 18:01:44 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamplaApp::Application.config.secret_token = '292d24ce54079329e133e8b47b217066bcdb1bc12bcdbd85ceb37bd318d709125e2f16a6dbf07a8ca405aecc18845f5b81c14c175178ec0a2f18891add362030'
